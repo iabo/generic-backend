eba6a858c0f19afa9fd57d80c58d790b14a6fb9c
Yabdul Abdala <yabdul@doorcounts.com>
2023-05-24 13:02:13 -0700

    fix generate_keys script

--- bin/generate_keys.sh
+++ bin/generate_keys.sh
@@ -10,10 +10,10 @@ fi
 if [ ! -f ./src/keys/JWT_RS256.key ]; then
   # Generate private key
   echo "Generating private key...";
-  ssh-keygen -t rsa -b 4096 -m PEM -f ../src/keys/JWT_RS256.key -q -N ""
+  ssh-keygen -t rsa -b 4096 -m PEM -f ./src/keys/JWT_RS256.key -q -N ""
   # Generate public key
   echo "Generating public key...";
-  openssl rsa -in ../src/keys/JWT_RS256.key -pubout -outform PEM -out ../src/keys/JWT_RS256.key.pub
+  openssl rsa -in ./src/keys/JWT_RS256.key -pubout -outform PEM -out ./src/keys/JWT_RS256.key.pub
 fi
 
 
